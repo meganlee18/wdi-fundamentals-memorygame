08fc10c85d0e790c13b34a2ba87228be534b6647
Megan <megan@Megans-MBP.lan>
2018-03-24 22:21:55 +1100

    added objects

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -21,7 +21,29 @@ console.log("Sorry, try again.");
 
 */
 
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: 'queen',
+		suit: 'diamond',
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage:"images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = []
 
 var checkForMatch = function () {
@@ -33,15 +55,19 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardsId) {
-	cardsInPlay.push (cards[cardsId])
-console.log("User flipped " + cards[cardsId])
+	cardsInPlay.push (cards[cardsId].rank)
+console.log("User flipped " + cards[cardsId].rank)
 checkForMatch();
 }
 
 flipCard(0);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
+
+
 flipCard(2);
-
-
+console.log(cards[2].cardImage); 
+console.log(cards[2].suit);
 
 
 
